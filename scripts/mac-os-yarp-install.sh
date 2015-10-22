b245fccdb37af5f8bcfc9f9afb7bbc6f5360553c
Francesco <francesco.nori@iit.it>
2015-10-22 19:01:03 +0900

    Added a script for installing yarp on MacOS. Mainly conceived for remembering the dependencies

---
+++ scripts/mac-os-yarp-install.sh
@@ -0,0 +1,57 @@
+if [ "$1" != "" ]; then
+export MY_CODE_DIR=$1
+    echo "The code will be installed in the directory " $MY_CODE_DIR
+    # build installation directories
+	mkdir $MY_CODE_DIR
+
+	# first install brew
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew doctor 
+	brew update
+	brew upgrade
+
+	# install yarp dependecies
+	brew tap homebrew/x11
+	brew tap homebrew/science
+	brew install qt5
+	brew linkapps qt5
+	brew install bash-completion
+	brew install yarp --only-dependencies --with-qt5
+
+    # get YARP code
+    cd $MY_CODE_DIR
+    git clone https://github.com/robotology/yarp.git
+    cd yarp
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DINSTALL_WITH_RPATH:STRING=ON -DCREATE_LIB_MATH:STRING=ON -DCREATE_GUIS:STRING=ON -DQt5_DIR:STRING=/usr/local/opt/qt5/lib/cmake/Qt5 -DCMAKE_CXX_FLAGS=-std=c++11
+    make
+    make install
+    cd ..
+    cd ..
+    git clone https://github.com/robotology/icub-main.git ./icub
+    cd icub
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DICUB_INSTALL_WITH_RPATH:STRING=ON 
+    make
+    make install
+	
+    echo '#YARP'                                                                   >> ~/.bash_profile
+    echo "CODE_DIR="$1                                                             >> ~/.bash_profile
+    echo 'export PATH=$PATH:$CODE_DIR/bin/bin'                                     >> ~/.bash_profile
+    echo 'export YARP_DIR=$CODE_DIR/yarp'                                          >> ~/.bash_profile
+    echo 'export YARP_DATA_DIRS=$CODE_DIR/bin/share/yarp:$CODE_DIR/bin/share/yarp' >> ~/.bash_profile
+    echo 'source $CODE_DIR/yarp/scripts/yarp_completion'                           >> ~/.bash_profile
+	echo 'source $(brew --prefix)/etc/bash_completion'                             >> ~/.bash_profile
+    . ~/.bash_profile
+else
+    echo "*****************************************************************"
+    echo "******Please provide a path where to install the software!*******"
+    echo "Use of this script is: ./yarp_install.sh /path/where/to/install"
+    echo "E.g. ./yarp_install.sh /home/user/Code"
+    echo "Contact: iron@liralab.it (Francesco Nori)"
+fi
+
+
+
