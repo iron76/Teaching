dd9a535753423304740b9d6b8e149ea875e0e9fa
Humanoid 2014 workshop <none@none.com>
2015-10-23 05:37:06 +0200

    scripts for ubunut yarp and icub added

---
+++ scripts/ubuntu-trusty-os-icub-install.sh
@@ -0,0 +1,32 @@
+if [ "$1" != "" ]; then
+export MY_CODE_DIR=$1
+    echo "The code will be installed in the directory " $MY_CODE_DIR
+    mkdir $MY_CODE_DIR
+
+    # install dependencies
+    sudo sh -c 'echo "deb http://www.icub.org/ubuntu trusty contrib/science" > /etc/apt/sources.list.d/icub.list'
+    sudo apt-get -y --force-yes install icub-common
+
+    cd $MY_CODE_DIR
+    git clone https://github.com/robotology/icub-main.git ./icub
+    cd icub
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DICUB_INSTALL_WITH_RPATH:STRING=ON
+    make
+    make install
+
+    echo 'source ~/.bash_profile'                                                  >> ~/.bashrc 
+    echo '#ICUB'                                                                   >> ~/.bash_profile
+    echo "CODE_DIR="$1                                                            >> ~/.bash_profile
+    echo 'export PATH=$PATH:$CODE_DIR/bin/bin'                                     >> ~/.bash_profile
+    echo "Executing bashrc..."
+    source ~/.bashrc
+    echo "...script terminated."
+else
+    echo ""
+    echo "Use of this script is: ./ubuntu-trusty-icub-install.sh /path/where/to/install"
+    echo "E.g. ./ubuntu-trusty-icub-install.sh /home/user/Code"
+    echo "Contact: iron@liralab.it (Francesco Nori)"
+    echo ""
+fi
--- scripts/ubuntu-trusty-os-yarp-install.sh
+++ scripts/ubuntu-trusty-os-yarp-install.sh
@@ -6,7 +6,7 @@ export MY_CODE_DIR=$1
     sudo apt-get -y install libace-dev
     sudo apt-get -y install cmake
     sudo apt-get -y install libgsl0-dev 
-    sudo apt-get install qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev \
+    sudo apt-get -y install qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev \
        qtdeclarative5-qtquick2-plugin qtdeclarative5-window-plugin \
        qtdeclarative5-qtmultimedia-plugin qtdeclarative5-controls-plugin \
        qtdeclarative5-dialogs-plugin libqt5svg5
@@ -17,15 +17,6 @@ export MY_CODE_DIR=$1
     mkdir build
     cd build
     cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DINSTALL_WITH_RPATH:STRING=ON -DCREATE_LIB_MATH:STRING=ON -DCREATE_GUIS:STRING=ON -DQt5_DIR:STRING=/usr/local/opt/qt5/lib/cmake/Qt5 -DCMAKE_CXX_FLAGS=-std=c++11
-    make
-    make install
-    cd ..
-    cd ..
-    git clone https://github.com/robotology/icub-main.git ./icub
-    cd icub
-    mkdir build
-    cd build
-    cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DICUB_INSTALL_WITH_RPATH:STRING=ON
     make
     make install
 
@@ -42,5 +33,5 @@ else
     echo "Use of this script is: ./ubuntu-trusty-yarp-install.sh /path/where/to/install"
     echo "E.g. ./ubuntu-trusty-yarp-install.sh /home/user/Code"
     echo "Contact: iron@liralab.it (Francesco Nori)"
-	echo ""
+    echo ""
 fi
